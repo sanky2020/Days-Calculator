5a80e687c0009dcc89e77b8ec4af55bf0d2f5a1b
sankyk2020 <69984129+sanky2020@users.noreply.github.com>
2022-05-26 15:09:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,14 @@ let element = document.querySelector("[name=sk]");
 let h1 = document.getElementById("h1")
 let h2 = document.getElementById("h2")
 let dateTag = document.querySelector('h3')
+let p = document.getElementsByTagName('p');
+
+let lastPTag = p[p.length-1];
+
+lastPTag.innerText = "Hit Submit to see Last Date as on calender:"
 
 let date= new Date();
+
 // controlled
 element.addEventListener('input',(e)=>{
     h1.innerText= e.target.value
@@ -16,6 +22,8 @@ function buttonClick(){
     date.setDate(date.getDate()+ parseInt(element.value));
 
     dateTag.innerHTML = date;
+    
+    lastPTag.innerText = "Last Date as on calender:";
 
 }
 dateTag.innerHTML = date;
